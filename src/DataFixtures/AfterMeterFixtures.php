dcb60c38b505fa99b195035fb9b3c95aa46f3485
Vang Jeannette <vangjeannette@gmail.com>
2019-08-12 16:43:35 +0200

    création du fichier aftermeterfixtures

---
+++ src/DataFixtures/AfterMeterFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\AfterMeter;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class AfterMeterFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($i = 0; $i < 5; $i++) {
+            $aftermeter = new AfterMeter();
+            $aftermeter->setMaterial($faker->word);
+            $aftermeter->setDiameter($faker->randomDigit);
+            $aftermeter->setScrewthread($faker->word);
+            $aftermeter->setThread($faker->sentence(3, true));
+            $aftermeter->setAccuracy(($faker->sentence(10, true)));
+            $aftermeter->setPicture($faker->image());
+            $manager->persist($aftermeter);
+        }
+        $manager->flush();
+    }
+}
