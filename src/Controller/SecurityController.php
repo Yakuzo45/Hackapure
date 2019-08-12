b950de691074a88e09c110dff27bfa7751ea435c
tomtom-p <pecoutt@hotmail.fr>
2019-08-12 16:09:56 +0200

    add routing

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,14 +1,20 @@
 <?php
 
+namespace App\Controller;
+
 use App\Entity\AdminUser;
 use App\Form\AdminUserType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
+    /**
+     * @Route("/", name="app_register")
+     */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $admin = new AdminUser();
--- src/Form/AdminUserType.php
+++ src/Form/AdminUserType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\AdminUser;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,18 @@ class AdminUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email :',
+                'attr' => ['placeholder' => 'Email']
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe doivent correspondre',
+                'options' => ['attr' => ['class' => 'password-field', 'placeholder' => 'Mot de Passe']],
+                'required' => true,
+                'first_options' => ['label' => 'Mot de Passe'],
+                'second_options' => ['label' => 'Confirmez le mot de passe']
+            ])
         ;
     }
 
