acf0bff21ea7683a3763e7187fd127423cec418d
tomtom-p <pecoutt@hotmail.fr>
2019-08-12 15:11:45 +0200

    add register controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Entity\AdminUser;
+use App\Form\AdminUserType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class SecurityController extends AbstractController
+{
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $admin = new AdminUser();
+        $form = $this->createForm(AdminUserType::class, $admin);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $admin->setPassword($passwordEncoder->encodePassword(
+                $admin,
+                $admin->getPassword()
+            ));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($admin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('Admin/register.html.twig', [
+            'admin' => $admin,
+            'form' => $form->createView()
+        ]);
+
+    }
+}
