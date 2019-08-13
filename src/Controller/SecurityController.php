340b5b7b91d60e5e699159c73601ef226bd069d5
tomtom-p <pecoutt@hotmail.fr>
2019-08-13 13:31:29 +0200

    add index admin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\AdminUser;
+use App\Form\AdminUserEditType;
 use App\Form\AdminUserType;
+use App\Repository\AdminUserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -14,29 +16,74 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
+     * @Route("/admin", name="admin_user_index", methods={"GET"})
+     */
+    public function index(AdminUserRepository $adminUserRepository): Response
+    {
+        return $this->render('Admin/index.html.twig', [
+            'admin_users' => $adminUserRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, AdminUser $adminUser): Response
+    {
+        $form = $this->createForm(AdminUserEditType::class, $adminUser);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+            $this->addFlash('success', 'l\'utilisateur ' . $adminUser->getEmail() . ' a bien été modifié');
+
+            return $this->redirectToRoute('admin_user_index');
+        }
+
+        return $this->render('Admin/edit.html.twig', [
+            'admin_user' => $adminUser,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_user_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, AdminUser $adminUser): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$adminUser->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($adminUser);
+            $entityManager->flush();
+            $this->addFlash('success', 'l\'utilisateur ' . $adminUser->getEmail() . ' a bien été supprimé');
+        }
+
+        return $this->redirectToRoute('admin_user_index');
+    }
+    /**
      * CreateAdminUser
      * @Route("/admin/register", name="app_register")
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
-        $admin = new AdminUser();
-        $form = $this->createForm(AdminUserType::class, $admin);
+        $adminUser = new AdminUser();
+        $form = $this->createForm(AdminUserType::class, $adminUser);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $admin->setPassword($passwordEncoder->encodePassword(
-                $admin,
-                $admin->getPassword()
+            $adminUser->setPassword($passwordEncoder->encodePassword(
+                $adminUser,
+                $adminUser->getPassword()
             ));
             $em = $this->getDoctrine()->getManager();
-            $em->persist($admin);
+            $em->persist($adminUser);
             $em->flush();
-            $this->addFlash('success', 'l\'utilisateur ' . $admin->getEmail() . ' a bien été ajouté');
+            $this->addFlash('success', 'l\'utilisateur ' . $adminUser->getEmail() . ' a bien été ajouté');
             return $this->redirectToRoute('app_register');
         }
 
         return $this->render('Admin/register.html.twig', [
-            'admin' => $admin,
+            'admin' => $adminUser,
             'form' => $form->createView()
         ]);
     }
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ Encore
      */
     .addEntry('mainJs', './assets/js/main.js')
     .addEntry('searchAddress', './assets/js/searchAddressAutocompletion.js')
-    .addStyleEntry('register', './assets/scss/register.scss')
+    .addStyleEntry('userAdmin', './assets/scss/userAdmin.scss')
     .addStyleEntry('mainCss', './assets/scss/main.scss')
     .addStyleEntry('login', './assets/scss/login.scss')
     //.addEntry('page1', './assets/js/page1.js')
